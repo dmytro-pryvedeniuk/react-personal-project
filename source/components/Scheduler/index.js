5522a254afca78186168e00d6e97dfb814fe4908
Dmytro Pryvedeniuk <dmytro.pryvedeniuk@gmail.com>
2018-12-05 00:22:17 +0200

    Initial UI version

--- source/components/Scheduler/index.js
+++ source/components/Scheduler/index.js
@@ -4,12 +4,54 @@ import React, { Component } from 'react';
 // Instruments
 import Styles from './styles.m.css';
 import { api } from '../../REST'; // ! Импорт модуля API должен иметь именно такой вид (import { api } from '../../REST')
+import Checkbox from '../../theme/assets/Checkbox';
+import Task from '../Task';
 
 export default class Scheduler extends Component {
-    render () {
+
+    state = {
+        newTaskMessage:  '',
+        tasksFilter:     '',
+        isTasksFetching: false,
+
+        tasks: [
+            {'id': '1', message: 'Task A', completed: 'true', favorite: 'true' },
+            {'id': '2', message: 'Task B', completed: 'false', favorite: 'true' },
+            {'id': '3', message: 'Task C', completed: 'false', favorite: 'true' }
+        ]  
+    }
+
+    render() {
+        const {tasks} = this.state; 
+
+        console.log(tasks);
+        var tasksJSX = tasks.map((task) => 
+            <Task key = {task.id} {...task}></Task>
+        );
+
         return (
-            <section className = { Styles.scheduler }>
-                Планировщик: стартовая точка
+            <section className={Styles.scheduler}>
+                <main>
+                    <header>
+                        <h1>Планировщик задач</h1>
+                        <input placeholder="Поиск" type="search" />
+                    </header>
+                    <section>
+                        <form>
+                            <input placeholder="Описание моей новой задачи" type="text" />
+                            <button>Добавить задачу</button>
+                        </form>
+                        <ul>
+                            {tasksJSX}
+                        </ul>
+                    </section>
+                    <footer>
+                        <div>
+                            <Checkbox color1='#363636' color2='#fff' />
+                        </div>
+                        <span className={Styles.completeAllTasks}>Все задачи выполнены</span>
+                    </footer>
+                </main>
             </section>
         );
     }
--- source/components/Task/index.js
+++ source/components/Task/index.js
@@ -3,8 +3,13 @@ import React, { PureComponent } from 'react';
 
 // Instruments
 import Styles from './styles.m.css';
+import Checkbox from '../../theme/assets/Checkbox';
+import Star from '../../theme/assets/Star';
+import Edit from '../../theme/assets/Edit';
+import Remove from '../../theme/assets/Remove';
 
 export default class Task extends PureComponent {
+
     _getTaskShape = ({
         id = this.props.id,
         completed = this.props.completed,
@@ -17,7 +22,18 @@ export default class Task extends PureComponent {
         message,
     });
 
-    render () {
-        return <li className = { Styles.task }>Задача: стартовая точка</li>;
+    render() {
+        var shape = this._getTaskShape({});
+        return (<li className={Styles.task}>
+            <div className ={Styles.content}>
+               <Checkbox className={Styles.toggleTaskCompletedState} color1='#3B8EF3' color2='#FFF'></Checkbox>
+               <span>{shape.message}</span> 
+            </div>
+            <div className ={Styles.actions}>
+                <Star inlineBlock className = {Styles.toggleTaskFavoriteState}></Star>
+                <Edit inlineBlock className = {Styles.updateTaskMessageOnClick}></Edit>
+                <Remove inlineBlock></Remove>
+            </div>
+        </li>);
     }
 }
